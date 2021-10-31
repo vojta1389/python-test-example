8517c940c81b1d44ee9afcbd7df4986238f485ba
Vojta <zapletal.vojta@gmail.com>
2021-10-31 19:49:22 +0100

    Initial commit

---
+++ src/script.py
@@ -0,0 +1,8 @@
+"""Python script for testing purpose only.
+
+This is setup just to test github tests and flake8.
+"""
+
+def hello():
+    """This is hello world function""""
+    return "Hello, World!"
---
+++ test/test.py
@@ -0,0 +1,4 @@
+from script import hello
+
+def test_hello:
+    assert hello() == "Hello, World!"
