a00cf30e4e268abf1e3b9c98b31946dfb42c028f
Vojta <zapletal.vojta@gmail.com>
2021-10-31 20:14:29 +0100

    Flake8 syntax bugs fixed

--- src/script.py
+++ src/script.py
@@ -3,6 +3,7 @@
 This is setup just to test github tests and flake8.
 """
 
+
 def hello():
     """This is hello world function"""
     return "Hello, World!"
--- test/test.py
+++ test/test.py
@@ -1,4 +1,5 @@
 from script import hello
 
+
 def test_hello():
     assert hello() == "Hello, World!"
